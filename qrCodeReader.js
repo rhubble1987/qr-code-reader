939db3e7f849256ddcd8fb3aaaf1142eacded7f8
Ryan Hubble <rhubble1987@gmail.com>
2020-12-21 23:10:33 -0500

    Fixed logic to display start route button

--- qrCodeReader.js
+++ qrCodeReader.js
@@ -5,10 +5,13 @@ const canvasElement = document.getElementById("qr-canvas");
 const canvas = canvasElement.getContext("2d");
 
 const qrResult = document.getElementById("qr-result");
-//const outputData = document.getElementById("outputData");
 const btnScanQR = document.getElementById("btn-scan-qr");
 const startRouteBtn = document.getElementById("start-route");
 const allCheckedInEl = document.getElementById("all-checked-in");
+
+let checkedInOrders = 0;
+
+
 
 const orders = [
 {
@@ -51,8 +54,7 @@ const orders = [
 function loadOrders() {
   for (h = 0; h < orders.length; h++) {
     qrResult.insertAdjacentHTML('afterbegin', 
-    `
-    <h5>${orders[h].lastName}, ${orders[h].firstName}  <span class="order-status" data-order-id="${orders[h].id}">${orders[h].display}</span></h5>
+    `<h5>${orders[h].lastName}, ${orders[h].firstName}  <span class="order-status" data-order-id="${orders[h].id}">${orders[h].display}</span></h5>
     <hr>
     `);
   }
@@ -68,19 +70,31 @@ function clearOrders() {
   let currentOrders = document.getElementById("qr-result");
   while (currentOrders.firstChild) {
   currentOrders.removeChild(currentOrders.firstChild);
+  }
 }
-}
+
+  function allCheckedIn() {
+    btnScanQR.hidden = true;
+    qrResult.hidden = true;
+    allCheckedInEl.hidden = false;
+  }
 
 function pickUp(res) {
   let notAssigned = 0;
   for (i = 0; i < orders.length; i++) {
-    if (res == orders[i].id) {
+    if (res == orders[i].id && orders[i].status === 'not-picked-up') {
       orders[i].status = 'picked-up';
       orders[i].scannedResult = res;
       orders[i].display = '&#9989;';
       i = orders.length;
+      checkedInOrders++;
       clearOrders();
       loadOrders();
+      
+      console.log(checkedInOrders);
+      
+    } else if (res == orders[i].id && orders[i].status === 'picked-up') {
+      alert('You have already scanned this order.');
     } else {
       notAssigned++;
     }
@@ -88,57 +102,11 @@ function pickUp(res) {
   if (notAssigned === orders.length) {
     alert('This order is not assigned to you. Please return it to the pharmacy.');
   }
-}
-
-//function pickUp(res) {
-  //const assignedOrder = orders.status.includes(res);
-  /* const assignedOrder = orders[i].id.includes(res);
-  console.log(assignedOrder); */
-  /* for (i = 0; i < 0; i++) { 
-    console.log(orders[i].id);
-    if (res == orders[i].id) {
-      orders[i].status = 'picked-up';
-      orders[i].display = '&#9989;';
-      i = orders.length;
-      } else {
-        alert('This order is not assigned to you. Please return it to the pharmacy.');
-      }
-      
-    }
-      clearOrders();
-      updateOrders();
-      allPickedUp(); 
-  } */
-    
-
-/* function updateStatus() {
-  const statusElement = document.getElementsByClassName('order-status').getAttribute('data-order-id');
-  console.log(statusElement);
-  for (i = 0; i < orders.length; i++) {
-    if (orders[i].id == statusId) {
-      i = orders.length;
-      this.statusElement.innerText = '&#x2705;';
-      console.log(this.statusElement.innerText);
-    }
+  if (checkedInOrders === orders.length) {
+    allCheckedIn()
   }
-}    */ 
-
-//This function checks to see if all orders have been picked up
-function allPickedUp() { 
-  let ordersCheckedIn = 0;
-  for (k = 0; k < orders.length; k++) {
-    if (orders[k].status === 'picked up') {
-      ordersCheckedIn++;
-    }
-  }
-  if (ordersCheckedIn === orders.length) {
-    btnScanQR.setAttribute('style','display: none');
-    qrResult.setAttribute('style','display: none;');
-    allPickedUp.setAttribute('style','display: initial;');
   
-  }
-} 
-    
+}    
 
 let scanning = false;
 
@@ -193,5 +161,8 @@ function tick() {
     }
   }
 
+  
+
+  
   
   
